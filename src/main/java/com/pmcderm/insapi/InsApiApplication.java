395d2e37dfb1fa15353243691e80bd6986647fe8
patrick mcdermott <patrickmcdermott@patricks-MBP.fios-router.home>
2020-10-31 16:50:42 -0400

    first commit

---
+++ src/main/java/com/pmcderm/insapi/InsApiApplication.java
@@ -0,0 +1,13 @@
+package com.pmcderm.insapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pmcderm/insapi/InsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmcderm.insapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
